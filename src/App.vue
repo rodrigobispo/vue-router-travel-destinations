26e1fad4772cd670776172eaa587bed37e9704d4
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-03 00:13:29 -0300

    Refatoração, isolando componente de navegação.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
-
+  <TheNavigation/>
   <div class="container">
     <router-view />
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import TheNavigation from '@/components/TheNavigation.vue';
+
+export default defineComponent({
+  components: {
+    TheNavigation
+  }
+})
+</script>
